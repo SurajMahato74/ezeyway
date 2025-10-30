aaa641e1666090fa7cc0924838d43b19e6b483e9
SurajMahato74 <surajmahato9823@gmail.com>
2025-10-30 08:56:02 +0545

    udated front

---
+++ dist/assets/index-CDTVOiNY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CeaUs-K3.js","assets/index-BLLT3Dgq.js","assets/index.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-BLLT3Dgq.js";const _=p("App",{web:()=>r(()=>import("./web-CeaUs-K3.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
